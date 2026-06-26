ef6a6eef7f20429ec18032959840c2c9fc8a6b09
Revanth Edapa <162419696+edaparevanth@users.noreply.github.com>
2026-06-26 22:06:42 +0530

    Add files via upload

---
+++ Week1/PL\SQL /Exercise3/Exercise3_StoredProcedures.sql
@@ -0,0 +1,70 @@
+-- SENARIO-1 PL/SQL block:
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+AS
+BEGIN
+    UPDATE Accounts
+    SET Balance = Balance + (Balance * 0.01)
+    WHERE AccountType = 'Savings';
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Monthly interest processed.');
+END;
+/
+-- verification:
+SELECT * FROM Accounts;
+
+-- SENARIO-2 PL/SQL block:
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus (
+    p_Department IN VARCHAR2,
+    p_Bonus IN NUMBER
+)
+AS
+BEGIN
+    UPDATE Employees
+    SET Salary = Salary + (Salary * p_Bonus / 100)
+    WHERE Department = p_Department;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Bonus applied successfully.');
+END;
+/
+-- verification:
+SELECT * FROM Employees;
+
+-- SENARIO-3 PL/SQL block:
+CREATE OR REPLACE PROCEDURE TransferFunds (
+    p_FromAccount IN NUMBER,
+    p_ToAccount IN NUMBER,
+    p_Amount IN NUMBER
+)
+AS
+    v_Balance NUMBER;
+BEGIN
+    SELECT Balance
+    INTO v_Balance
+    FROM Accounts
+    WHERE AccountID = p_FromAccount;
+
+    IF v_Balance >= p_Amount THEN
+
+        UPDATE Accounts
+        SET Balance = Balance - p_Amount
+        WHERE AccountID = p_FromAccount;
+
+        UPDATE Accounts
+        SET Balance = Balance + p_Amount
+        WHERE AccountID = p_ToAccount;
+
+        COMMIT;
+
+        DBMS_OUTPUT.PUT_LINE('Transfer Successful');
+
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Insufficient Balance');
+    END IF;
+END;
+/
+-- verification:
+SELECT * FROM Accounts;
